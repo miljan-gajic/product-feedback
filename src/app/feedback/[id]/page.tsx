078badcd1f033de00180bc7a16985f8af5ee0908
Miljan Gajic <miljangajic@mac.speedport.ip>
2025-07-28 12:39:30 +0200

    adding a divider line

--- src/app/feedback/[id]/page.tsx
+++ src/app/feedback/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { getProductFeedbackById } from '@/app/api/productFeedback';
 import { SecondaryNav } from '@/components/SecondaryNav/SecondaryNav';
 import { Card } from '@/components/Card/Card';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Feedback } from '@/components/Feedback/Feedback';
 import { Comment } from '@/components/Comment/Comment';
 import { calculateTotalComments } from '@/utils/calculateTotalComments';
@@ -29,19 +29,21 @@ export default async function FeedbackPage({
           feedbackId={+id}
         />
       </Card>
-      <Card cs="w-full mt-4">
+      <Card cs="w-full mt-4 pt-8 pb-10">
         <h1 className="text-secondary-text text-xl font-bold mb-6">
           <span className="pr-2">{totalComments}</span>
           {totalComments && totalComments === 1 ? 'Comment' : 'Comments'}
         </h1>
         {comments?.map(({ id, content, user, replies }) => (
-          <Comment
-            key={id}
-            content={content}
-            user={user}
-            replies={replies}
-            totalComments={totalComments}
-          />
+          <Fragment key={id}>
+            <Comment
+              content={content}
+              user={user}
+              replies={replies}
+              totalComments={totalComments}
+            />
+            <div className="w-full border-t border-main-border-fade mx-4 my-6 last:border-0"></div>
+          </Fragment>
         ))}
       </Card>
     </>
